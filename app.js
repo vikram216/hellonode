aa4ecced4bbadc1300ebb763d2ec2591cda7828c
vikram saraswathi <visarasw@in.ibm.com>
2017-08-04 16:46:37 +0530

    Adding app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+app.get('/', function(req, res){
+  res.status(200).send('Hello, world!').end();
+});
+var port=process.env.PORT || 8080;
+
+app.listen(port, functio(){
+  console.log('App listening on '+port);
+  console.log('Press Ctrl+C to quit.');
+});
